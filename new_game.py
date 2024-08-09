6ab43a11298924bfffd3c927fff62faa9a8a113d
Taha-Tahmasb <tahatahmasbypour@gmail.com>
2024-08-09 23:31:04 +0330

    new fix

--- new_game.py
+++ new_game.py
@@ -8,15 +8,30 @@ curses.cbreak()
 stdscr.keypad(True)
 stdscr.nodelay(True)
 world = [] 
+food = []
+score = 0 
 player_l = player_c = 0  
+def random_place():
+    a = random.randint(0,maxl) 
+    b = random.randint(0,maxc)
+    while world[a][b] != ord(" "):
+        a = random.randint(0,maxl) 
+        b = random.randint(0,maxc)
+    return a,b 
+        
+
 def init():
     global player_l , player_c
-    for i in range(maxl):
+    for i in range(-1 , maxl + 1):
         world.append([])
-        for j in range(maxc):
+        for j in range(-1, maxc + 1):
             world[i].append(ord(" ") if random.random() > 0.03 else ord("."))
-    player_l = random.randint(0,maxl)
-    player_c = random.randint(0,maxc)
+    player_l , player_c = random_place()
+    for i in range(10):
+        fl ,fc = random_place()
+        fa = random.randint(1000,10000)
+        food.append((fl,fc,fa))
+
 def in_range(a,min,max):
     if a < min :
         return min 
@@ -28,25 +43,37 @@ def in_range(a,min,max):
 
 def move(c):
     global player_l , player_c
-    if c == "w":
+    if c == "w" and world[player_l - 1][player_c] != ord("."):
         player_l -= 1 
-    elif c == "s":
+    elif c == "s" and world[player_l + 1][player_c] != ord("."):
         player_l += 1
-    elif c == "a":
+    elif c == "a" and world[player_l][player_c - 1] != ord("."):
         player_c -= 1
-    elif c == "d":
+    elif c == "d" and world[player_l][player_c + 1] != ord("."):
         player_c += 1 
-    player_l = in_range(player_l , 0,maxl)
-    player_c = in_range(player_c ,0,maxc)
+    player_l = in_range(player_l , 0,maxl - 1)
+    player_c = in_range(player_c ,0,maxc - 1)
 
-
+def check_food():
+    global food , score
+    for f in food :
+        fl , fc , fa = f
+        if fl == player_l and fc == player_c :
+            score += 10 
+            food.remove(f)
+            world[fl][fc] = ord(" ")
+            fa = random.randint(1000,10000)
+            food.append((fl,fc,fa))
 
 
 def draw():
     for i in range(maxl):
         for j in range(maxc):
             stdscr.addch(i, j, world[i][j])
-    stdscr.addch(player_l, player_c, ord("x"))
+    for c in food:
+        fl , fc , fa = c
+        stdscr.addch(fl, fc, "🍎")
+    stdscr.addch(player_l, player_c, "🚀")
     stdscr.refresh()
 init()
 playing = True 
@@ -59,6 +86,7 @@ while playing:
         move(c)
     elif c == 'q':
         playing = False
+    check_food()
     draw()
 
 
