e3e43f6fae0038f7d70479383c8fdcd7cb28b361
Taha-Tahmasb <tahatahmasbypour@gmail.com>
2024-08-09 22:52:23 +0330

    adding move function

--- new_game.py
+++ new_game.py
@@ -6,21 +6,39 @@ maxc = curses.COLS
 curses.noecho()
 curses.cbreak()
 stdscr.keypad(True)
-curses.nodelay( True)
-
-
+stdscr.nodelay(True)
 world = [] 
-player_1 = player_c = 0  
+player_l = player_c = 0  
 def init():
-    global player_1 , player_c
+    global player_l , player_c
     for i in range(maxl):
         world.append([])
         for j in range(maxc):
             world[i].append(ord(" ") if random.random() > 0.03 else ord("."))
-    player_1 = random.randint(0,maxl)
+    player_l = random.randint(0,maxl)
     player_c = random.randint(0,maxc)
+def in_range(a,min,max):
+    if a < min :
+        return min 
+    if a > max :
+        return max
+    return a 
+
+
+
 def move(c):
-    pass
+    global player_l , player_c
+    if c == "w":
+        player_l -= 1 
+    elif c == "s":
+        player_l += 1
+    elif c == "a":
+        player_c -= 1
+    elif c == "d":
+        player_c += 1 
+    player_l = in_range(player_l , 0,maxl)
+    player_c = in_range(player_c ,0,maxc)
+
 
 
 
@@ -28,20 +46,21 @@ def draw():
     for i in range(maxl):
         for j in range(maxc):
             stdscr.addch(i, j, world[i][j])
-    stdscr.addch(player_1, player_c, ord("x"))
+    stdscr.addch(player_l, player_c, ord("x"))
     stdscr.refresh()
 init()
 playing = True 
 while playing:
-    c = stdscr.getkey()
-    if c in ['asdw']:
+    try :
+        c = stdscr.getkey()
+    except :
+        c = ""
+    if c in 'asdw':
         move(c)
     elif c == 'q':
         playing = False
-
-
-
     draw()
 
 
-stdscr.getkey()
+stdscr.clear()
+stdscr.refresh()
