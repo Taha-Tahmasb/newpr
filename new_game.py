c4469cce40f18363cd5e15d14f2ed8dc78e89ae2
Taha-Tahmasb <tahatahmasbypour@gmail.com>
2024-08-09 20:34:04 +0330

    new loop

--- new_game.py
+++ new_game.py
@@ -1,4 +1,5 @@
 import curses
+import random 
 stdscr = curses.initscr()
 maxl = curses.LINES - 1 
 maxc = curses.COLS
@@ -12,7 +13,7 @@ def init():
     for i in range(maxl):
         world.append([])
         for j in range(maxc):
-            world[i].append(ord("."))
+            world[i].append(ord(" ") if random.random() > 0.03 else ord("."))
 def draw():
     for i in range(maxl):
         for j in range(maxc):
